bbe59d254682deb8e7b5369ef49c31b3eda1eeb4
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-10-03 14:33:55 +0200

    modified

--- Crabank/Database/Models/BankAdvisor.cs
+++ Crabank/Database/Models/BankAdvisor.cs
@@ -7,5 +7,5 @@ public class BankAdvisor
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-    public int PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; }
 }
