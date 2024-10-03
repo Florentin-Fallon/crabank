254aebebda502f6f185a67d600fdd7b302ffef91
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2024-10-03 12:16:28 +0200

    made Iban as primary key for bank account model

--- Crabank/Database/Models/BankAccount.cs
+++ Crabank/Database/Models/BankAccount.cs
@@ -1,12 +1,14 @@
-﻿namespace Crabank.Database.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace Crabank.Database.Models;
+
+[PrimaryKey(propertyName: "Iban")]
 public class BankAccount
 {
-    public int Id { get; set; }
+    public string Iban { get; set; }
     
     public string Name { get; set; }
     public double Amount { get; set; }
-    public string Iban { get; set; }
     public double CreditLimit { get; set; }
     public string BankAdvisorName { get; set; }
 }
