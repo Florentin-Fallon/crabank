716c4353354ac48def565a23dc185be5963ac77e
geoffrey <geoffrey.coutant@ynov.com>
2024-11-14 10:56:36 +0100

    notification

---
+++ Crabank/Utilities/Notifications.cs
@@ -0,0 +1,15 @@
+using System.Text;
+using System.Web;
+
+namespace Crabank.Utilities;
+
+public static class Notifications
+{
+    public static async Task SendAsync (string title,string message, params string[] emojis)
+    {
+        HttpClient client = new();
+        client.DefaultRequestHeaders.Add("Title", HttpUtility.HtmlEncode(title));
+        client.DefaultRequestHeaders.Add("Tags", string.Join(',', emojis)); 
+        await client.PostAsync("https://ntfy.sh/kayoukoubeme", new StringContent(message, Encoding.UTF8));
+    }
+}
